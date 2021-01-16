28c84ca7a05b7cd3d45ce773f2f45fbe2c19c0af
Joeri Bakker <bakkerjoeri@gmail.com>
2021-01-16 22:51:23 +0100

    PERFECT KID

--- Kid.gd
+++ Kid.gd
@@ -1,19 +1,35 @@
 extends Node2D
 
 export(PackedScene) var dirt_scene
+export var number_of_dirt_spots: int = 4
+export var dirt_per_spot: int = 8
+
+var my_dirts = 0
+var is_clean = false
 
 func _ready():
-	for p in range(4):
+	for p in range(number_of_dirt_spots):
 		var dirt_group_position = Vector2(
-			rand_range(position.x, position.x + 48),
-			rand_range(position.y, position.y + 48)
+			rand_range(-22, 22),
+			rand_range(-22, 22)
 		)
 
-		for q in range(8):
+		for q in range(dirt_per_spot):
 			var dirt = dirt_scene.instance()
 			dirt.position = dirt_group_position + Vector2(
 				rand_range(-3, 3),
 				rand_range(-3, 3)
 			)
 			add_child(dirt)
-			
+			my_dirts += 1
+			dirt.connect("cleaned", self, "_on_Dirt_cleaned")
+
+func _process(delta):
+	if my_dirts == 0 && !is_clean:
+		is_clean = true
+		print("PERFECT KID!")
+
+func _on_Dirt_cleaned():
+	my_dirts -= 1
+	print("bye bye dirt, new dirt count is " + str(my_dirts))
+	
