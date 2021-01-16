3ddd1d3faae5af42a04285a9fca02b1db18059cd
Joeri Bakker <bakkerjoeri@gmail.com>
2021-01-16 22:24:45 +0100

    Dirt?

--- Dirt.gd
+++ Dirt.gd
@@ -1,5 +1,16 @@
-extends Node2D
+extends Area2D
+
+const colors = [
+	Color("#2f5753"),
+	Color("#283540"),
+	Color("#4f1d4c"),
+	Color("#781d4f"),
+	Color("#52333f"),
+	Color("#7d3833"),
+]
 
 func _ready():
-	randomize()
-	$AnimatedSprite.set_frame(randi() % $AnimatedSprite.frames.get_frame_count("default"))
+	$ColorRect.color = colors[randi() % colors.size()]
+
+func _on_Dirt_area_entered(area):
+	print("Dirt be scrubbed from area")
--- Kid.gd
+++ Kid.gd
@@ -3,9 +3,17 @@ extends Node2D
 export(PackedScene) var dirt_scene
 
 func _ready():
-	position = Vector2(100, 100)
-	
-	for n in range(4):
-		var dirt = dirt_scene.instance()
-		add_child(dirt)
-		dirt.position = Vector2(rand_range(-16, 2), rand_range(-24, 8))
+	for p in range(4):
+		var dirt_group_position = Vector2(
+			rand_range(position.x, position.x + 48),
+			rand_range(position.y, position.y + 48)
+		)
+
+		for q in range(8):
+			var dirt = dirt_scene.instance()
+			dirt.position = dirt_group_position + Vector2(
+				rand_range(-3, 3),
+				rand_range(-3, 3)
+			)
+			add_child(dirt)
+			
