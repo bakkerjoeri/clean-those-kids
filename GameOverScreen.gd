a7cb3296b89d118669e4bd1ac61624428a4b115f
Joeri Bakker <bakkerjoeri@gmail.com>
2021-01-17 23:15:56 +0100

    Game over screen works

--- GameOverScreen.gd
+++ GameOverScreen.gd
@@ -4,7 +4,7 @@ func set_score(score: int):
 	$Score.text = str(score) + " points"
 
 func _on_TryAgainButton_pressed():
-	print("try again")
+	get_tree().change_scene("res://Main.tscn")
 
 func _on_MainMenuButton_pressed():
-	print("to main menu, please")
+	get_tree().change_scene("res://StartScreen.tscn")
