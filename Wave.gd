7c932bf46d83b635ac8ea7d57c0f502a294fe537
eaipool <e.a.i.pool@tudelft.nl>
2021-01-17 14:35:26 +0100

    use "self.", more pretty

--- Wave.gd
+++ Wave.gd
@@ -5,18 +5,17 @@ signal add_kid
 
 var kid_count
 
-
-func _init(var _kid_count):
-	kid_count = _kid_count
+func _init(var kid_count):
+	self.kid_count = kid_count
 	
 func on_Kid_cleaned():
-	kid_count -= 1
+	self.kid_count -= 1
 
 func is_wave_finished():
-	return kid_count <= 0	
+	return self.kid_count <= 0	
 		
 func build_wave():
-	for _k in range(kid_count):
+	for _k in range(self.kid_count):
 		emit_signal("add_kid")
 
 			
